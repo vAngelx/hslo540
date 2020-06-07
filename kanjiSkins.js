8af0d5ca017e342a047550f3c2ced47ba36cdc83
vAngelx <56030529+vAngelx@users.noreply.github.com>
2020-06-06 23:27:45 -0700

    Add files via upload

---
+++ kanjiSkins.js
@@ -0,0 +1,74 @@
+{
+  "巰": {
+    "username": "test001",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/test001.png"
+  },
+  "曷": {
+    "username": "YueAgar_c",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/YueAgar_c.png"
+  },
+  "皌": {
+    "username": "Nativus",
+    "skinurl": "https://i.imgur.com/mnwgSM9.png"
+  },
+  "뀮": {
+    "username": "Li_Ren",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/Li_Ren.png"
+  },
+  "눍": {
+    "username": "fragpro",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/fragpro.jpg"
+  },
+  "翆": {
+    "username": "fragpro1",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/fragpro1.jpg"
+  },
+  "Ȋ": {
+    "username": "Star666",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/Star666.png"
+  },
+  "䧵": {
+    "username": "gmtan87",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/gmtan87.png"
+  },
+  "罰": {
+    "username": "fragprodragon1",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/fragprodragon1.jpg"
+  },
+  "⨠": {
+    "username": "fragprodragon2",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/fragprodragon2.jpg"
+  },
+  "䀶": {
+    "username": "bennyso",
+    "skinurl": "https://i.imgur.com/45plFYx.jpg"
+  },
+  "苝": {
+    "username": "CCCBBB",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/CCCBBB.jpg"
+  },
+  "嶣": {
+    "username": "BBBCCC",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/BBBCCC.jpg"
+  },
+  "촉": {
+    "username": "MigsE7",
+    "skinurl": "https://i.imgur.com/sXnJzzE.png"
+  },
+  "ᆛ": {
+    "username": "Exarch",
+    "skinurl": "https://i.imgur.com/PLFcFQ9.png"
+  },
+  "䯂": {
+    "username": "Exarch2",
+    "skinurl": "https://i.imgur.com/aRAaFmB.png"
+  },
+  "돠": {
+    "username": "fragflws1",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/fragflws1.jpg"
+  },
+  "": {
+    "username": "fragflws2",
+    "skinurl": "https://raw.githubusercontent.com/vAngelx/hslo540/master/skins_kanji/fragflws2.jpg"
+  }
+}
